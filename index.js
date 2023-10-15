3707c823cdff2e33742f35820880908b4b4b45c9
sesar <54498315+sesar2@users.noreply.github.com>
2023-10-15 15:13:41 +0200

    b

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ config()
 
 app.get('/', async (req, res)=> {
     const products = await getProducts() 
-    const markup = products.map((p)=>`<a style="display:block; color: black; border: solid 2px black; margin: 20px; padding: 10px" href="/test">${p.title} - ${p.price} kr</a>`).join(' ')
+    const markup = `<div style="display: flex; padding: 100px; flex-wrap: wrap; gap: 20px"> ${products.map((p)=>`
+    <div style="border: 1px solid black; padding: 10px; width: 240px; height: 430px; display: flex; flex-direction: column; align-items: center;"  >
+        <img style="height: 200px; width: 200px; object-fit: cover;" src=${p.image} alt="">
+        <div style="width: 100%;">
+            <h3>${p.title}</h3>
+            <h3> ${p.price} kr</h3>
+            <a href="/products/${p.id}" style="text-decoration: none; color: black;">
+            <button>Buy now!</button>
+            </a>
+        </div>
+    </div>
+    `)}</div>`
     res.send(markup)
 })
 
