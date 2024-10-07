95f206b277f5aad858fbbe9d7de21d154497227c
sesar <54498315+sesar2@users.noreply.github.com>
2024-10-07 16:35:03 +0200

    test

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ config()
 
 app.get('/', async (req, res)=> {
     const products = await getProducts() 
-    const markup = `    <div style="display: flex; flex-direction: column; justify-content: center; font-family: 'Courier New', Courier, monospace;">
+    const markup = `<div style="display: flex; flex-direction: column; justify-content: center; font-family: 'Courier New', Courier, monospace;">
         <h1 style="text-align: center;">My Klarna shop </h1>
 
         <div style="display: flex; padding: 100px; flex-wrap: wrap; gap: 20px; font-family: Courier New, Geneva, Tahoma, sans-serif;"> ${products.map((p)=>`
             <div style=" border: 1px solid black; min-height: 430px; display: flex; flex-direction: column; align-items: center; border-radius: 15px; background: rgb(235, 235, 235); width: 240px;;" >
-                <img style="height: 240px; width: 240px; object-fit: cover; border-radius: 15px 15px 0 0;" src="${p.image}" alt="">
+                <img style="min-height: 280px; width: 280px; object-fit: fill; border-radius: 15px 15px 0 0;" src="${p.image}" alt="">
                 
                 <div style="gap: 30px; display: flex; flex-direction: column; height: 100%; width: 80%; padding: 20px; justify-content: space-between;">
                     <div style="display: flex; flex-direction: column; gap: 20px; max-width: 240px;">
                         <h3 style="margin: 0; font-size: 20px; font-weight: 900; word-wrap: break-word; max-width: 100%;">
                             ${p.title}
                         </h3>
-                        <h3 style="margin: 0; font-size: 14px; color: rgba(95, 95, 95, 0.841);"> ${p.price} kr</h3>
+                        <h3 style="margin: 0; font-size: 18px; color: rgba(95, 95, 95, 0.841);"> ${p.price} kr</h3>
                     </div>
                     <a href="/products/${p.id}" style="text-decoration: none; color: black; align-self: flex-end; font-weight: bold;">
                         <button style="border: 0.5px solid black; height: 30px; background: #FFA8CD; font-weight: bold; border-radius: 5px;">Buy now with Klarna!</button>
